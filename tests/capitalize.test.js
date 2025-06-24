e452931707a931b5989ccbbe2e2724bb27d121d0
Oleg Bogachev <bolegcarrick@gmail.com>
2025-06-24 20:17:08 +0300

    tests added assert lib

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,14 @@
+import { strict as assert } from "node:assert";
 import capitalize from "..//src/capitalize.js";
 
-if (capitalize("hello") !== "Hello") {
-  throw new Error("Test failed: capitalize('hello') should return 'Hello'");
-}
+// if (capitalize("hello") !== "Hello") {
+//   throw new Error("Test failed: capitalize('hello') should return 'Hello'");
+// }
 
-if (capitalize("") !== "") {
-  throw new Error("Test failed: capitalize('') should return ''");
-}
+// if (capitalize("") !== "") {
+//   throw new Error("Test failed: capitalize('') should return ''");
+// }
+assert.equal(capitalize(""), "");
 
+assert.equal(capitalize("hello"), "Hello");
 console.log("All tests passed!");
